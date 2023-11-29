a59097669a2ad2acf5d6498b6b39a0e3b0150555
LavrinDmitrovich <dmitrylavrinovich@gmail.com>
2023-11-29 13:32:49 +0200

    Add project files.

---
+++ 2. basic queue operations/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _2._basic_queue_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int pushElem = parameters[0];
+            int popElem = parameters[1];
+            int searchElem = parameters[2];
+
+            var queue = new Queue<int>(values.Take(pushElem));
+
+            while (popElem > 0 & queue.Count > 0)
+            {
+                queue.Dequeue();
+                popElem--;
+            }
+
+            if (queue.Count == 0)
+            { Console.WriteLine("0"); }
+
+            else if (queue.Contains(searchElem)) { Console.WriteLine("true"); }
+            else Console.WriteLine(queue.Min());
+        }
+    }
+}
---
+++ 3. max and min element/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _3._max_and_min_element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < num; i++) 
+            { 
+            
+                var comm = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                int first = comm[0];
+
+                if (first == 1)  stack.Push(comm[1]); 
+                else if (first == 2) stack.Pop();
+                else if (first == 3 && stack.Count > 0) Console.WriteLine(stack.Max());
+                else if (first == 4 && stack.Count > 0) Console.WriteLine(stack.Min());
+
+
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+
+        }
+    }
+}
